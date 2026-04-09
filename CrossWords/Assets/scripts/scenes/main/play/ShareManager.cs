4e765fab7f78039c26eb5187843c1c34efcc92a5
Peter Robinson <drinkcoffee@eml.c>
2026-04-09 14:12:22 +1000

    Improved share capability

--- CrossWords/Assets/scripts/scenes/main/play/ShareManager.cs
+++ CrossWords/Assets/scripts/scenes/main/play/ShareManager.cs
@@ -56,8 +56,12 @@ namespace CrossWords {
                     }
                 }
                 msg += "Score: " + score;
+
+msg += check();
+
                 // DebugPanel.SetActive(true);
                 // DebugText.text = msg;
+                AuditLog.Log($"Shared: {msg}");
                 SunShineNativeShare.instance.ShareText(msg, msg);
             }
             else
@@ -70,33 +74,131 @@ namespace CrossWords {
             StringBuilder sbi = new StringBuilder(solution);
             StringBuilder sbo = new StringBuilder();
             
-            // Implement Mathematical Monospace A (\U0001D670) as a Unicode Surrogate Pair.
-            // See: https://www.russellcottrell.com/greek/utilities/SurrogatePairCalculator.htm
-            // char mathematicalMonospaceAHigh = '\uD835';
-            // char mathematicalMonospaceALow = '\uDE70';
-            char smallSpace = '\u2009';
-            char figureSpace = '\u2007';
             for (int i = 0; i < solution.Length; i++)
             {
                 char c = sbi[i];
                 if (c == ' ')
                 {
-                    sbo.Append(figureSpace);
+                    sbo.Append('\u3000');
                 }
                 else
                 {
-                    // int charVal = c - 'A';
-                    // char low = (char) (mathematicalMonospaceALow + charVal);
-                    // string mathematicalMonospace = new string(new char[] { mathematicalMonospaceAHigh, low });
-                    // sbo.Append(mathematicalMonospace);
-                    sbo.Append(c);
-                    if (c == 'I')
-                    {
-                        sbo.Append(smallSpace);
-                    }
+                    int charVal = c - 'A';
+//                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F130 + charVal);
+//                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F150 + charVal);
+                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F170 + charVal);
+                    sbo.Append(mathMonoSpaceC);
                 }
             }
+
+
             return sbo.ToString();
         }
+
+        private string check() {
+            StringBuilder sbo = new StringBuilder();
+            sbo.Append("\n");
+
+            int num = 11;
+            
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    int charVal = i;
+                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F130 + charVal);
+                    sbo.Append(mathMonoSpaceC);
+                }
+                sbo.Append("X\n");
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    int charVal = i;
+                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F150 + charVal);
+                    sbo.Append(mathMonoSpaceC);
+                }
+                sbo.Append("X\n");
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    int charVal = i;
+                    string mathMonoSpaceC = char.ConvertFromUtf32(0x1F150 + charVal);
+                    sbo.Append(mathMonoSpaceC);
+                }
+                sbo.Append("X\n");
+            }
+
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u3000');
+            }
+            sbo.Append("X3000\n");
+
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u205F');
+            }
+            sbo.Append("X205F\n");
+
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u200A');
+            }
+            sbo.Append("X200A\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2009');
+            }
+            sbo.Append("X2009\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2008');
+            }
+            sbo.Append("X2008\n");
+
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append("\u2007");
+            }
+            sbo.Append("X2007\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2006');
+            }
+            sbo.Append("X2006\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2005');
+            }
+            sbo.Append("X2005\n");
+
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2003');
+            }
+            sbo.Append("X2003\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2002');
+            }
+            sbo.Append("X2002\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2001');
+            }
+            sbo.Append("X2001\n");
+            for (int j = 0; j < num; j++)
+            {
+                sbo.Append('\u2000');
+            }
+            sbo.Append("X2000\n");
+
+            return sbo.ToString();
+        }
+
     }
 }
