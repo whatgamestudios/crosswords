426a4bb54178947c4d52bd7667a51c57757e4651
Peter Robinson <drinkcoffee@eml.c>
2026-05-17 11:52:32 +1000

    Add new words

--- CrossWords/Assets/scripts/scenes/main/wordlist/NewWordsScreen.cs
+++ CrossWords/Assets/scripts/scenes/main/wordlist/NewWordsScreen.cs
@@ -16,6 +16,9 @@ namespace CrossWords {
             AuditLog.Log($"New Words screen");
             InfoText.text = 
                 "\n" +
+                "Words added in Worcadian version 2.3 (May 18, 2026):\n" +
+                "pj, pjs, fx, vj, oj\n" +
+                "\n" +
                 "Words added in Worcadian version 2.0 (May 10, 2026):\n" +
                 "karens, unfriends, wip\n" +
                 "\n" +
